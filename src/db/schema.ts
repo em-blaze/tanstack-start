146741697a360853c9b2faa6fd7ab94b56cbcda8
em-blaze <emblaze7@proton.me>
2026-03-16 11:26:36 +0200

    Initial commit

---
+++ src/db/schema.ts
@@ -0,0 +1,6 @@
+// import { integer, pgTable } from "drizzle-orm/pg-core"
+
+// export const usersTable = pgTable("users", {
+//   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+//   hero_id: integer().notNull().unique(),
+// })
